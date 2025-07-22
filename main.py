03e9991aabb425c3663894ce75bc81bebbaf2d9d
Mamaaaaaa123 <tawanft789@gmail.com>
2025-07-22 15:20:40 +0700

    Add code call string in main

--- main.py
+++ main.py
@@ -6,8 +6,15 @@
 # print(calculator.divide(100, 100))
 
 #3
-import lambda_calculator
-print(lambda_calculator.add(100, 100))
-print(lambda_calculator.substract(100, 100))
-print(lambda_calculator.multiply(100, 100))
-print(lambda_calculator.divide(100, 100))
+# import lambda_calculator
+# print(lambda_calculator.add(100, 100))
+# print(lambda_calculator.substract(100, 100))
+# print(lambda_calculator.multiply(100, 100))
+# print(lambda_calculator.divide(100, 100))
+
+#4
+import string_operations
+print(string_operations.reverse_string("OHHH yeah"))
+print(string_operations.capitalize_string("OHHH yeah"))
+print(string_operations.lowercase_string("OHHH yeah"))
+print(string_operations.uppercase_string("OHHH yeah"))
