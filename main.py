8a29a9d1bfaa7e75aa925d4caf2e2861f73c0c5b
Mamaaaaaa123 <tawanft789@gmail.com>
2025-07-22 15:34:21 +0700

    Organize my code into packages

--- main.py
+++ main.py
@@ -20,3 +20,18 @@
 # print(string_operations.uppercase_string("OHHH yeah"))
 
 #5
+import calculator
+print("Using calculator.py:")
+print("Addition:", calculator.add(10, 10))
+print("Subtraction:", calculator.substract(10, 10))
+print("Multiplication:", calculator.multiply(10, 10))
+print("Division:", calculator.divide(10, 10))
+
+import string_operations
+sample_string = "Ohhh yeah"
+print("\nUsing string_operations.py:")
+print("Original:", sample_string)
+print("Reversed:", string_operations.reverse_string(sample_string))
+print("Capitalized:", string_operations.capitalize_string(sample_string))
+print("Lowercase:", string_operations.lowercase_string(sample_string))
+print("Uppercase:", string_operations.uppercase_string(sample_string))
