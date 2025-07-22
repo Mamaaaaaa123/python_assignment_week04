ceea636c55720435febcb08f9e304f8b099cd7b7
Mamaaaaaa123 <tawanft789@gmail.com>
2025-07-22 15:10:43 +0700

    ADD code to call each lambda function with sample input and print

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #2
-import calculator
-print(calculator.add(100, 100))
-print(calculator.substract(100, 100))
-print(calculator.multiply(100, 100))
-print(calculator.divide(100, 100))
+# import calculator
+# print(calculator.add(100, 100))
+# print(calculator.substract(100, 100))
+# print(calculator.multiply(100, 100))
+# print(calculator.divide(100, 100))
+
+#3
+import lambda_calculator
+print(lambda_calculator.add(100, 100))
+print(lambda_calculator.substract(100, 100))
+print(lambda_calculator.multiply(100, 100))
+print(lambda_calculator.divide(100, 100))
